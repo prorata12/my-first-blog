09d7b3af841f541bf198bb202ef528accb1081c9
prorata12 <prorata@naver.com>
2017-05-13 11:21:36 +0900

    Changed the HTML for the site

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+def post_list(request):
+	return render(request, 'blog/post_list.html',{})
+def post_list2(request):
+	return render(request, 'blog/post_list2.html',{})
+
 # Create your views here.
